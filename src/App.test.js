60a16b2d5eac65eb8b6bb207ed0a5877674643e5
alexlionnel <alexlionnel@yahoo.fr>
2022-05-02 17:32:03 +0200

    Redux, stripe, netlify serverless function (#2)
    * add redux
    
    * add redux-thunk and redux-persist
    
    * add redux-saga
    
    * add stripe and netlify function
    
    * test
    
    * action error
    
    Co-authored-by: Alex SIEYADJEU <alex.sieyadjeu@externe.maif.fr>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { screen } from '@testing-library/react';
 
 test('renders learn react link', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
   expect(screen).toBeDefined();
 });
